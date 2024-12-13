6e5bb2a4b55abbc22ca696b794bb28fd1f0e6dba
Ayman <asuleihi@gmail.com>
2024-12-13 23:24:47 +0000

    Add content

--- src/frontend/src/views/Contact.tsx
+++ src/frontend/src/views/Contact.tsx
@@ -1,5 +1,7 @@
 import React, { ChangeEvent, FormEvent, useState } from "react"
 import { Input, Textarea, Button } from "@material-tailwind/react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'
 import { UtilsService } from "../client"
 import { AlertMessage } from "../components/AlertMessage"
 
@@ -41,40 +43,62 @@ const Contact: React.FC = () => {
             {alertContent}
           </AlertMessage>
         }
-        <form onSubmit={handleSubmit}>
-          <div className="space-y-4">
-            <Input
-              type="text"
-              label="Name"
-              value={formData.name}
-              onChange={handleChange}
-              name="name"
-              required
-            />
-            <Input
-              type="email"
-              label="Email"
-              value={formData.email}
-              onChange={handleChange}
-              name="email"
-              required
-            />
-            <Textarea
-              label="Message"
-              resize={true}
-              value={formData.message}
-              onChange={handleChange}
-              name="message"
-              required
-            />
-            <Button
-              type="submit"
-              variant="outlined"
-              ripple={false}
-              className="rounded border-gray-500 mt-5"
-            >Submit</Button>
+        <div className="flex flex-col md:flex-row">
+          <div className="md:pr-2 pb-2">
+            If you have any questions or inquiries, feel free to reach out via email at {" "}
+            <a 
+              href="mailto:contact@shebascaravan.com" 
+              className="text-gray-800 underline hover:text-gray-700"
+            >
+              contact@shebascaravan.com
+            </a>
+            , instagram at {" "}
+            <a
+              href="https://www.instagram.com/shebascaravan"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-gray-800 hover:text-gray-700 transition"
+              aria-label="Instagram"
+            >
+              @shebascaravan
+            </a> {" "}
+            or use the following form.
           </div>
-        </form>
+          <form className="flex w-full md:pl-2 pt-2" onSubmit={handleSubmit}>
+            <div className="space-y-4 w-full">
+              <Input
+                type="text"
+                label="Name"
+                value={formData.name}
+                onChange={handleChange}
+                name="name"
+                required
+              />
+              <Input
+                type="email"
+                label="Email"
+                value={formData.email}
+                onChange={handleChange}
+                name="email"
+                required
+              />
+              <Textarea
+                label="Message"
+                resize={true}
+                value={formData.message}
+                onChange={handleChange}
+                name="message"
+                required
+              />
+              <Button
+                type="submit"
+                variant="outlined"
+                ripple={false}
+                className="rounded border-gray-500 mt-5"
+              >Submit</Button>
+            </div>
+          </form>
+        </div>
       </div>
     </main>
   )
