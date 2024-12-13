4f4083736220fa3f86d1cb740d58358f6354ff86
Ayman <asuleihi@gmail.com>
2024-12-13 23:19:47 +0000

    Add motif, location, links, social

--- src/frontend/src/components/Footer.tsx
+++ src/frontend/src/components/Footer.tsx
@@ -1,11 +1,51 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'
 
 export const Footer: React.FC = () => {
   return (
-    <footer className="bg-gray-50 border-gray-200 border-t">
-      <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
-        <div className="text-center text-sm text-gray-800">
-          © {new Date().getFullYear()} Sheba's Caravan. All rights reserved.
+    <footer className="bg-gray-50 border-gray-200">
+      <div className="flex w-full items-center">
+        <div className="flex-1 border-b mr-4"></div>
+        <img className="h-3" src="/yemenite_motif_short_1.svg"></img>
+        <img className="h-3" src="/yemenite_motif_short_1.svg"></img>
+        <img className="h-3" src="/yemenite_motif_short_1.svg"></img>
+        <img className="h-3" src="/yemenite_motif_short_1.svg"></img>
+        <img className="h-3" src="/yemenite_lama.svg"></img>
+        <div className="flex-1 border-b ml-4"></div>
+      </div>
+      <div className="max-w-7xl mx-auto pb-4 px-4 sm:px-6 lg:px-8">
+        <div className="flex flex-col-reverse sm:flex-row justify-between items-center pt-2">
+          <div className="flex-1 text-center sm:text-left py-2">
+            <p className="text-gray-800 text-sm">London, UK</p>
+          </div>
+          <div className="flex-1 text-center sm:py-2">
+            <ul className="inline-flex space-x-4">
+              <li><Link to="/about" className="text-sm font-semi text-gray-800 hover:underline">About</Link></li>
+              <li><Link to="/care" className="text-sm font-semi text-gray-800 hover:underline">Care</Link></li>
+              <li><Link to="/delivery" className="text-sm font-semi text-gray-800 hover:underline">Delivery</Link></li>
+              <li><Link to="/contact" className="text-sm font-semi text-gray-800 hover:underline">Contact</Link></li>
+              <li><Link to="/terms" className="text-sm text-gray-800 hover:underline">Terms</Link></li>
+              <li><Link to="/privacy" className="text-sm text-gray-800 hover:underline">Privacy</Link></li>
+            </ul>
+          </div>
+          <div className="flex-1 flex justify-center sm:justify-end py-2">
+            <a
+              href="https://www.instagram.com/shebascaravan"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-gray-800 hover:text-gray-700 transition"
+              aria-label="Instagram"
+            >
+              <FontAwesomeIcon icon={faInstagram} className="w-5 h-5" />
+            </a>
+          </div>
+        </div>
+        <div className="text-center">
+          <p className="text-gray-800 text-sm">
+            © {new Date().getFullYear()} Sheba's Caravan. All rights reserved.
+          </p>
         </div>
       </div>
     </footer>
