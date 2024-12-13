5c350594653b45aa7e00d0e504a4a8f118df39d7
Ayman <asuleihi@gmail.com>
2024-12-13 23:26:23 +0000

    Add fonts, text-shadow

--- src/frontend/tailwind.config.js
+++ src/frontend/tailwind.config.js
@@ -1,4 +1,5 @@
-const withMT = require("@material-tailwind/react/utils/withMT");
+const withMT = require("@material-tailwind/react/utils/withMT")
+const plugin = require('tailwindcss/plugin')
 
 module.exports = withMT({
   content: [
@@ -6,12 +7,30 @@ module.exports = withMT({
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      sans: ["Montserrat", "sans-serif"],
+    },
     extend: {
       boxShadow: {
-        "crescent": "0 9px 0 0"
-      }
+        crescent: "0 9px 0 0",
+      },
+      textShadow: {
+        sm: '0 1px 2px var(--tw-shadow-color)',
+        DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+        lg: '0 8px 16px var(--tw-shadow-color)',
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          'text-shadow': (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme('textShadow') }
+      )
+    }),
+  ],
 })
-
