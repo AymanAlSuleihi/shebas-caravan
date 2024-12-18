faa17b24ec5a7981250deaaae2b1cee880b8d59c
Ayman <asuleihi@gmail.com>
2024-12-18 22:39:44 +0000

    Cleanup

--- src/frontend/src/views/Admin/Orders/OrderList.tsx
+++ src/frontend/src/views/Admin/Orders/OrderList.tsx
@@ -1,23 +1,14 @@
-import React, { useState, useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import { OrderOut } from "../../../client/models/OrderOut"
 import { OrdersService } from "../../../client"
-import {
-  MagnifyingGlassIcon,
-  ChevronUpDownIcon,
-} from "@heroicons/react/24/outline"
 import { EyeIcon, PencilIcon, UserPlusIcon } from "@heroicons/react/24/solid"
 import {
   Card,
   CardHeader,
-  Input,
   Typography,
   Button,
   CardBody,
   Chip,
-  CardFooter,
-  Tabs,
-  TabsHeader,
-  Tab,
   Avatar,
   IconButton,
   Tooltip,
@@ -26,23 +17,10 @@ import { Link } from "react-router-dom"
 import { useDataGrid } from "@refinedev/mui"
 import { DataGrid, GridColDef } from "@mui/x-data-grid"
 
-const formatDate = (date?: string) => {
-  if (date) {
-    const dateString = new Date(date)
-    return dateString.toISOString().split('T')[0]
-  }
-  return null
-}
-
-const TABLE_HEAD = ["id", "Ordered at", " Status", "Products", "Customer", "Amount", ""]
-
 const OrderList: React.FC = () => {
-  const [orders, setOrders] = useState<OrderOut[]>()
   const [orderStatus, setOrderStatus] = useState<Record<string, any>>()
 
   useEffect(() => {
-  //   OrdersService.ordersReadOrders({}).then(
-  //     (response) => setOrders(response))
     OrdersService.ordersOrderStatus().then(
       (response) => setOrderStatus(response))
   }, [])
@@ -63,8 +41,6 @@ const OrderList: React.FC = () => {
     onFilterModelChange,
     ...restDataGridProps
   } = dataGridProps
-
-  console.log(dataGridProps)
 
   const columns = React.useMemo<GridColDef<OrderOut>[]>(
     () => [
@@ -220,13 +196,6 @@ const OrderList: React.FC = () => {
     [orderStatus],
   )
 
-  // return (
-  //   <div style={{ padding:"4px" }}>
-  //     <h2>Products</h2>
-  //     <DataGrid {...dataGridProps} columns={columns} />
-  //   </div>
-  // )
-
   return (
     <main className="flex-grow bg-gray-50">
       <div className="max-w-7xl mx-auto py-6 px-5 sm:px-6 lg:px-8">
@@ -265,153 +234,6 @@ const OrderList: React.FC = () => {
               getRowHeight={() => 'auto'}
             />
           </CardBody>
-          {/* <CardBody className="overflow-auto px-0">
-            <table className="mt-4 w-full min-w-max table-auto text-left">
-              <thead>
-                <tr>
-                  {TABLE_HEAD.map((head, index) => (
-                    <th
-                      key={head}
-                      className="cursor-pointer border-y border-blue-gray-100 bg-blue-gray-50/50 p-4 transition-colors hover:bg-blue-gray-50"
-                    >
-                      <Typography
-                        variant="small"
-                        color="blue-gray"
-                        className="flex items-center justify-between gap-2 font-normal leading-none opacity-70"
-                      >
-                        {head}{" "}
-                        {index !== TABLE_HEAD.length - 1 && (
-                          <ChevronUpDownIcon strokeWidth={2} className="h-4 w-4" />
-                        )}
-                      </Typography>
-                    </th>
-                  ))}
-                </tr>
-              </thead>
-              <tbody>
-                {orderStatus && orders?.map(
-                  ({ id, amount, status, products, created_at, customer }, index) => {
-                    const isLast = index === orders.length - 1
-                    const classes = isLast
-                      ? "p-4"
-                      : "p-4 border-b border-blue-gray-50"
-                    const statusName = Object.keys(orderStatus).find((key) => orderStatus[key] == status)
-                    console.log(orderStatus)
-                    console.log(status)
-                    console.log(statusName)
-                    return (
-                      <tr key={id}>
-                        <td className={classes}>
-                          <Typography
-                            variant="small"
-                            color="blue-gray"
-                            className="font-normal"
-                          >
-                            {id}
-                          </Typography>
-                        </td>
-                        <td className={classes}>
-                          <Typography
-                            variant="small"
-                            color="blue-gray"
-                            className="font-normal"
-                          >
-                            {formatDate(created_at)}
-                          </Typography>
-                        </td>
-                        <td className={classes}>
-                          <div className="w-max">
-                            <Chip
-                              variant="ghost"
-                              size="sm"
-                              value={statusName}
-                              color={
-                                statusName?.toUpperCase() === "PENDING"
-                                  ? "gray"
-                                  : statusName === "PROCESSING"
-                                  ? "orange"
-                                  : statusName === "SHIPPED"
-                                  ? "blue"
-                                  : statusName === "DELIVERED"
-                                  ? "green"
-                                  : "gray"
-                              }
-                            />
-                          </div>
-                        </td>
-                        <td className={classes}>
-                          {products?.map((product) => (
-                            <div className="flex items-center gap-3">
-                              <div className="flex flex-col">
-                                <Typography
-                                  variant="small"
-                                  color="blue-gray"
-                                  className="font-normal"
-                                >
-                                  {product.name}
-                                </Typography>
-                              </div>
-                            </div>
-                          ))}
-                        </td>
-                        <td className={classes}>
-                          <div className="flex items-center gap-3">
-                            <div className="flex flex-col">
-                              <Typography
-                                variant="small"
-                                color="blue-gray"
-                                className="font-normal"
-                              >
-                                {customer?.first_name} {customer?.last_name}
-                              </Typography>
-                            </div>
-                          </div>
-                        </td>
-                        <td className={classes}>
-                          <Typography
-                            variant="small"
-                            color="blue-gray"
-                            className="font-normal"
-                          >
-                            £{amount}
-                          </Typography>
-                        </td>
-                        <td className={classes}>
-                          <Tooltip content="View Order">
-                            <Link to={`/admin/orders/${id}`}>
-                              <IconButton variant="text">
-                                <EyeIcon className="h-4 w-4" />
-                              </IconButton>
-                            </Link>
-                          </Tooltip>
-                          <Tooltip content="Edit Order">
-                            <Link to={`/admin/orders/${id}/edit`}>
-                              <IconButton variant="text">
-                                <PencilIcon className="h-4 w-4" />
-                              </IconButton>
-                            </Link>
-                          </Tooltip>
-                        </td>
-                      </tr>
-                    )
-                  },
-                )}
-              </tbody>
-            </table>
-          </CardBody>
-          <CardFooter className="flex items-center justify-between border-t border-blue-gray-50 p-4">
-            <Typography variant="small" color="blue-gray" className="font-normal">
-              Page 1 of 10
-            </Typography>
-            <div className="flex gap-2">
-              <Button variant="outlined" size="sm">
-                Previous
-              </Button>
-              <Button variant="outlined" size="sm">
-                Next
-              </Button>
-            </div>
-          </CardFooter> */}
         </Card>
       </div>
     </main>
--- src/frontend/src/views/Admin/Products/ProductList.tsx
+++ src/frontend/src/views/Admin/Products/ProductList.tsx
@@ -1,23 +1,12 @@
-import React, { useState, useEffect } from "react"
+import React from "react"
 import { ProductOut } from "../../../client/models/ProductOut"
-import { ProductsService } from "../../../client"
-import {
-  MagnifyingGlassIcon,
-  ChevronUpDownIcon,
-} from "@heroicons/react/24/outline"
 import { EyeIcon, PencilIcon, PlusIcon } from "@heroicons/react/24/solid"
 import {
   Card,
   CardHeader,
-  Input,
   Typography,
   Button,
   CardBody,
-  Chip,
-  CardFooter,
-  Tabs,
-  TabsHeader,
-  Tab,
   Avatar,
   IconButton,
   Tooltip,
@@ -26,17 +15,7 @@ import { Link } from "react-router-dom"
 import { useDataGrid } from "@refinedev/mui"
 import { DataGrid, GridColDef } from "@mui/x-data-grid"
 
-const TABLE_HEAD = ["id", "Name", "Sku", "Quantity", "Cost", "Price", "Type", ""]
-
 const ProductList: React.FC = () => {
-  const [products, setProducts] = useState<ProductOut[]>()
-
-  useEffect(() => {
-    ProductsService.productsReadProducts({}).then(
-      (response) => setProducts(response.products)
-    )
-  }, [])
-
   const { dataGridProps } = useDataGrid<ProductOut>({
     resource: "products",
   })
@@ -170,23 +149,6 @@ const ProductList: React.FC = () => {
                 </Link>
               </div>
             </div>
-            {/* <div className="flex flex-col items-center justify-between gap-4 md:flex-row">
-              <Tabs value="all" className="w-full md:w-max">
-                <TabsHeader>
-                  {TABS.map(({ label, value }) => (
-                    <Tab key={value} value={value}>
-                      &nbsp&nbsp{label}&nbsp&nbsp
-                    </Tab>
-                  ))}
-                </TabsHeader>
-              </Tabs>
-              <div className="w-full md:w-72">
-                <Input
-                  label="Search"
-                  icon={<MagnifyingGlassIcon className="h-5 w-5" />}
-                />
-              </div>
-            </div> */}
           </CardHeader>
           <CardBody className="overflow-auto px-0">
             <DataGrid
@@ -205,145 +167,6 @@ const ProductList: React.FC = () => {
               getRowHeight={() => 75}
             />
           </CardBody>
-            {/* <table className="mt-4 w-full min-w-max table-auto text-left">
-              <thead>
-                <tr>
-                  {TABLE_HEAD.map((head, index) => (
-                    <th
-                      key={head}
-                      className="cursor-pointer border-y border-blue-gray-100 bg-blue-gray-50/50 p-4 transition-colors hover:bg-blue-gray-50"
-                    >
-                      <Typography
-                        variant="small"
-                        color="blue-gray"
-                        className="flex items-center justify-between gap-2 font-normal leading-none opacity-70"
-                      >
-                        {head}{" "}
-                        {index !== TABLE_HEAD.length - 1 && (
-                          <ChevronUpDownIcon strokeWidth={2} className="h-4 w-4" />
-                        )}
-                      </Typography>
-                    </th>
-                  ))}
-                </tr>
-              </thead>
-              <tbody>
-                {products?.map(
-                  ({ id, images, name, sku, quantity, cost, price, type, url_key }, index) => {
-                    const isLast = index === products.length - 1
-                    const classes = isLast
-                      ? "p-4"
-                      : "p-4 border-b border-blue-gray-50"
-                    return (
-                      <tr key={id}>
-                        <td className={classes}>
-                          <Typography
-                            variant="small"
-                            color="blue-gray"
-                            className="font-normal"
-                          >
-                            {id}
-                          </Typography>
-                        </td>
-                        <td className={classes}>
-                          <div className="flex items-center gap-3">
-                            <Avatar src={images ? `/public/products/${sku}/${images?.[0]}` : undefined} alt={name} size="sm" />
-                            <div className="flex flex-col">
-                              <Typography
-                                variant="small"
-                                color="blue-gray"
-                                className="font-normal"
-                              >
-                                {name}
-                              </Typography>
-                            </div>
-                          </div>
-                        </td>
-                        <td className={classes}>
-                          <div className="flex items-center gap-3">
-                            <div className="flex flex-col">
-                              <Typography
-                                variant="small"
-                                color="blue-gray"
-                                className="font-normal"
-                              >
-                                {sku}
-                              </Typography>
-                            </div>
-                          </div>
-                        </td>
-                        <td className={classes}>
-                          <Typography
-                            variant="small"
-                            color="blue-gray"
-                            className="font-normal"
-                          >
-                            {quantity}
-                          </Typography>
-                        </td>
-                        <td className={classes}>
-                          <Typography
-                            variant="small"
-                            color="blue-gray"
-                            className="font-normal"
-                          >
-                            £{cost}
-                          </Typography>
-                        </td>
-                        <td className={classes}>
-                          <Typography
-                            variant="small"
-                            color="blue-gray"
-                            className="font-normal"
-                          >
-                            £{price}
-                          </Typography>
-                        </td>
-                        <td className={classes}>
-                          <Typography
-                            variant="small"
-                            color="blue-gray"
-                            className="font-normal"
-                          >
-                            {type}
-                          </Typography>
-                        </td>
-                        <td className={classes}>
-                          <Tooltip content="View Product" >
-                            <Link to={`/treasure/${url_key}`} target="_blank">
-                              <IconButton variant="text">
-                                <EyeIcon className="h-4 w-4" />
-                              </IconButton>
-                            </Link>
-                          </Tooltip>
-                          <Tooltip content="Edit Product">
-                            <Link to={`/admin/products/${id}/edit`}>
-                              <IconButton variant="text">
-                                <PencilIcon className="h-4 w-4" />
-                              </IconButton>
-                            </Link>
-                          </Tooltip>
-                        </td>
-                      </tr>
-                    )
-                  },
-                )}
-              </tbody>
-            </table>
-          </CardBody>
-          <CardFooter className="flex items-center justify-between border-t border-blue-gray-50 p-4">
-            <Typography variant="small" color="blue-gray" className="font-normal">
-              Page 1 of 10
-            </Typography>
-            <div className="flex gap-2">
-              <Button variant="outlined" size="sm">
-                Previous
-              </Button>
-              <Button variant="outlined" size="sm">
-                Next
-              </Button>
-            </div>
-          </CardFooter> */}
         </Card>
       </div>
     </main>
