95e672ebb9db6d6ec8bca2bd7649d7f4337555c3
Ayman <asuleihi@gmail.com>
2024-12-20 00:21:06 +0000

    Add customer view

---
+++ src/frontend/src/views/Admin/Customers/CustomerView.tsx
@@ -0,0 +1,198 @@
+import React, { useState, useEffect } from "react"
+import { useParams } from "react-router-dom"
+import { CustomerOut } from "../../../client/models/CustomerOut"
+import { CustomersService, OrdersService } from "../../../client"
+import formatDate from "../../../utils/utils"
+import {
+  Card,
+  CardHeader,
+  Typography,
+  Button,
+  CardBody,
+  Chip,
+} from "@material-tailwind/react"
+import { EyeIcon, PencilIcon } from "@heroicons/react/24/solid"
+import { Link } from "react-router-dom"
+
+const CustomerView: React.FC = () => {
+  const [customer, setCustomer] = useState<CustomerOut>()
+  const [orderStatusList, setOrderStatusList] = useState<Record<string, number>>()
+  const { customerId = "" } = useParams<string>()
+
+  useEffect(() => {
+    CustomersService.customersReadCustomerById({
+      customerId: parseInt(customerId)
+    }).then((response) => setCustomer(response))
+  }, [customerId])
+
+  useEffect(() => {
+    OrdersService.ordersOrderStatus().then(
+      (response) => setOrderStatusList(response))
+  }, [])
+
+  const getOrderStatusName = (status: number) => {
+    return Object.keys(orderStatusList || {}).find((key) => orderStatusList![key] === status) || ""
+  }
+
+  return (
+    <main className="flex-grow bg-gray-50">
+      <div className="max-w-7xl mx-auto py-6 px-5 sm:px-6 lg:px-8">
+        <Card className="h-full w-full">
+          <CardHeader floated={false} shadow={false} className="rounded-none">
+            <div className="flex items-center justify-between gap-8">
+              <div>
+                <Typography variant="h5" color="blue-gray">
+                  Customer #{customer?.id}
+                </Typography>
+              </div>
+              <div className="flex shrink-0 flex-col gap-2 sm:flex-row">
+                <Link to={`/admin/customers/${customer?.id}/edit`}>
+                  <Button className="flex items-center gap-3 shadow-none hover:shadow-md" color="black" size="sm">
+                    <PencilIcon strokeWidth={2} className="h-4 w-4" /> Edit
+                  </Button>
+                </Link>
+              </div>
+            </div>
+          </CardHeader>
+          <CardBody className="overflow-auto px-0">
+            <div className="grid grid-cols-1 gap-4">
+              <Card shadow={false} className="rounded">
+                <CardBody className="px-4 pb-4 pt-2">
+                  <p><strong>Name:</strong> {customer?.first_name} {customer?.last_name}</p>
+                  <p><strong>Email:</strong> {customer?.email}</p>
+                </CardBody>
+              </Card>
+              <Card shadow={false} className="rounded">
+                <CardHeader floated={false} shadow={false}>
+                  <Typography variant="h6" color="blue-gray">
+                    Orders
+                  </Typography>
+                </CardHeader>
+                <CardBody className="px-0 pb-4 pt-2">
+                  {customer?.orders?.length ? (
+                    <table className="w-full min-w-max table-auto text-left">
+                      <thead>
+                        <tr>
+                          <th className="cursor-pointer border-y border-blue-gray-100 bg-blue-gray-50/50 p-4 transition-colors hover:bg-blue-gray-50">
+                            <Typography
+                              variant="small"
+                              color="blue-gray"
+                              className="flex items-center justify-between gap-2 font-normal leading-none opacity-70"
+                            >
+                              Order ID
+                            </Typography>
+                          </th>
+                          <th className="cursor-pointer border-y border-blue-gray-100 bg-blue-gray-50/50 p-4 transition-colors hover:bg-blue-gray-50">
+                            <Typography
+                              variant="small"
+                              color="blue-gray"
+                              className="flex items-center justify-between gap-2 font-normal leading-none opacity-70"
+                            >
+                              Date
+                            </Typography>
+                          </th>
+                          <th className="cursor-pointer border-y border-blue-gray-100 bg-blue-gray-50/50 p-4 transition-colors hover:bg-blue-gray-50">
+                            <Typography
+                              variant="small"
+                              color="blue-gray"
+                              className="flex items-center justify-between gap-2 font-normal leading-none opacity-70"
+                            >
+                              Amount
+                            </Typography>
+                          </th>
+                          <th className="cursor-pointer border-y border-blue-gray-100 bg-blue-gray-50/50 p-4 transition-colors hover:bg-blue-gray-50">
+                            <Typography
+                              variant="small"
+                              color="blue-gray"
+                              className="flex items-center justify-between gap-2 font-normal leading-none opacity-70"
+                            >
+                              Status
+                            </Typography>
+                          </th>
+                          <th className="cursor-pointer border-y border-blue-gray-100 bg-blue-gray-50/50 p-4 transition-colors hover:bg-blue-gray-50">
+                            <Typography
+                              variant="small"
+                              color="blue-gray"
+                              className="flex items-center justify-between gap-2 font-normal leading-none opacity-70"
+                            >
+                              Actions
+                            </Typography>
+                          </th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {customer.orders.map((order) => (
+                          <tr key={order.id}>
+                            <td className="p-4 border-b border-blue-gray-50">
+                              <Typography
+                                variant="small"
+                                color="blue-gray"
+                                className="font-normal"
+                              >
+                                {order.id}
+                              </Typography>
+                            </td>
+                            <td className="p-4 border-b border-blue-gray-50">
+                              <Typography
+                                variant="small"
+                                color="blue-gray"
+                                className="font-normal"
+                              >
+                                {formatDate(order.created_at)}
+                              </Typography>
+                            </td>
+                            <td className="p-4 border-b border-blue-gray-50">
+                              <Typography
+                                variant="small"
+                                color="blue-gray"
+                                className="font-normal"
+                              >
+                                £{order.amount}
+                              </Typography>
+                            </td>
+                            <td className="p-4 border-b border-blue-gray-50">
+                              <Chip
+                                className="w-fit"
+                                variant="ghost"
+                                size="sm"
+                                value={getOrderStatusName(order.status)}
+                                color={
+                                  getOrderStatusName(order.status)?.toUpperCase() === "PENDING"
+                                    ? "gray"
+                                    : getOrderStatusName(order.status) === "PROCESSING"
+                                    ? "orange"
+                                    : getOrderStatusName(order.status) === "SHIPPED"
+                                    ? "blue"
+                                    : getOrderStatusName(order.status) === "DELIVERED"
+                                    ? "green"
+                                    : "gray"
+                                }
+                              />
+                            </td>
+                            <td className="p-4 border-b border-blue-gray-50">
+                              <Link to={`/admin/orders/${order.id}`}>
+                                <Button size="sm" variant="text">
+                                  <EyeIcon className="h-4 w-4" />
+                                </Button>
+                              </Link>
+                            </td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+                  ) : (
+                    <Typography variant="body2" color="blue-gray">
+                      No orders available.
+                    </Typography>
+                  )}
+                </CardBody>
+              </Card>
+            </div>
+          </CardBody>
+        </Card>
+      </div>
+    </main>
+  )
+}
+
+export default CustomerView
