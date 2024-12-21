0c39ee032d352e576c9515a6a948ae58f2d352d2
Ayman <asuleihi@gmail.com>
2024-12-21 23:57:14 +0000

    Add product filters progressive image and skeleton

--- src/frontend/src/views/Categories.tsx
+++ src/frontend/src/views/Categories.tsx
@@ -1,27 +1,364 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
+import ProgressiveImage from "../components/ProgressiveImage"
+import { ProductOut, ProductsService, CategoriesService, CategoryOut } from "../client"
+import { Button, Input, Select, Option, Typography } from "@material-tailwind/react"
+import { BarsArrowUpIcon, BarsArrowDownIcon } from "@heroicons/react/24/solid"
+import { FunnelIcon } from "@heroicons/react/24/outline"
+import { Drawer, IconButton } from "@material-tailwind/react"
+import CategorySkeleton from "../components/Skeletons/CategorySkeleton"
+
 
 const Categories: React.FC = () => {
+  const [products, setProducts] = useState<ProductOut[]>([])
+  const [filteredProducts, setFilteredProducts] = useState<ProductOut[]>([])
+  const [categories, setCategories] = useState<CategoryOut[]>([])
+  const [filterName, setFilterName] = useState<string>("")
+  const [filterType, setFilterType] = useState<string>("")
+  const [filterCategory, setFilterCategory] = useState<string>("")
+  const [sortOrder, setSortOrder] = useState<string>("asc")
+  const [sortField, setSortField] = useState<string>("price")
+  const [drawerOpen, setDrawerOpen] = useState(false)
+  const [loaded, setLoaded] = useState(false)
+
+  const toggleDrawer = () => {
+    setDrawerOpen(!drawerOpen)
+  }
+
+  useEffect(() => {
+    ProductsService.productsReadProducts({}).then((response) => {
+      setProducts(response.products)
+      setFilteredProducts(response.products)
+    }).catch((error) => {
+      console.error("Failed to fetch products:", error)
+    })
+
+    CategoriesService.categoriesReadCategories({}).then((response) => {
+      setCategories(response.categories ?? [])
+    }).catch((error) => {
+      console.error("Failed to fetch categories:", error)
+      setCategories([])
+    })
+  }, [])
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 540) {
+        setDrawerOpen(false)
+      }
+    }
+
+    window.addEventListener("resize", handleResize)
+    return () => {
+      window.removeEventListener("resize", handleResize)
+    }
+  }, [])
+
+  useEffect(() => {
+    let updatedProducts = [...products]
+
+    if (filterName) {
+      updatedProducts = updatedProducts.filter(product =>
+        product.name.toLowerCase().includes(filterName.toLowerCase())
+      )
+    }
+
+    if (filterType) {
+      updatedProducts = updatedProducts.filter(product =>
+        product.type.toLowerCase().includes(filterType.toLowerCase())
+      )
+    }
+
+    if (filterCategory) {
+      updatedProducts = updatedProducts.filter(product =>
+        product.categories?.some(category => category.name.toLowerCase().includes(filterCategory.toLowerCase()))
+      )
+    }
+
+    if (sortField === "price") {
+      updatedProducts.sort((a, b) => sortOrder === "asc" ? a.price - b.price : b.price - a.price)
+    } else if (sortField === "name") {
+      updatedProducts.sort((a, b) => sortOrder === "asc" ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name))
+    }
+
+    setFilteredProducts(updatedProducts)
+    setLoaded(true)
+  }, [filterName, filterType, filterCategory, sortOrder, sortField, products])
+
   return (
-    <main className="flex-grow bg-gray-50">
-      <div className="max-w-7xl mx-auto py-6 px-5 sm:px-6 lg:px-8">
-        <h2 className="my-5 font-semibold text-2xl mb-4">Treasures</h2>
-        <div className="flex flex-wrap items-center justify-center gap-4">
-          <Link to="/treasures/pendants">
-            <div className="bg-white border aspect-square w-48 relative rounded hover:scale-[1.03] transition-transform">
-              <img src="crescent_amulet_1.png" className=""></img>
-              <div className="text-center font-semibold">Pendants</div>
+    <>
+      { loaded ? (
+      <main className="flex-grow bg-gray-50">
+        <div className="max-w-7xl mx-auto py-6 px-5 sm:px-6 lg:px-8">
+          <div className="flex flex-wrap justify-between items-center mb-4">
+            <Typography variant="h2" className="flex font-semibold text-2xl pr-5">Treasures</Typography>
+            <IconButton
+              onClick={toggleDrawer}
+              className="sm:hidden w-20 max-w-40 rounded"
+              variant="text"
+            >
+              <div className="flex items-center">
+                <Typography className="text-xs mr-1">Filter</Typography>
+                <FunnelIcon className="h-5 w-5 pl-1 text-gray-900" />
+              </div>
+            </IconButton>
+            <Drawer
+              open={drawerOpen}
+              onClose={toggleDrawer}
+              className="sm:hidden"
+              placement="bottom"
+            >
+              <div className="px-4 pt-4">
+                <div className="flex flex-col mb-4">
+                  <Input
+                    id="filter-name"
+                    type="text"
+                    value={filterName}
+                    onChange={(e) => setFilterName(e.target.value)}
+                    variant="standard"
+                    labelProps={{ className: "after:border-b-[1px]" }}
+                    containerProps={{ className: "" }}
+                    label="Search By Name"
+                  />
+                </div>
+                <div className="flex flex-col mb-4">
+                  <Select
+                    id="filter-category"
+                    value={filterCategory}
+                    onChange={(e) => {
+                      // console.log("Selected category:", e)
+                      setFilterCategory(e ?? "")
+                    }}
+                    variant="standard"
+                    labelProps={{ className: "after:border-b-[1px]" }}
+                    containerProps={{ className: "" }}
+                    label="Category"
+                    >
+                    <Option value="">All Categories</Option>
+                    <Option value="Pendants">Pendants</Option>
+                    <Option value="Rings">Rings</Option>
+                    {Array.isArray(categories) && categories.map(category => (
+                      <Option key={category.id} value={category.name}>{category.name ?? "gsd"}</Option>
+                    ))}
+                  </Select>
+                </div>
+                <div className="flex flex-row mb-4 space-x-4">
+                  <div className="flex flex-col max-w-full w-full">
+                    <Select
+                      id="sort-field"
+                      value={sortField}
+                      onChange={(e) => setSortField(e)}
+                      variant="standard"
+                      labelProps={{ className: "after:border-b-[1px] max-w-full w-full" }}
+                      containerProps={{ className: "" }}
+                      label="Sort by"
+                    >
+                      <Option value="price">Price</Option>
+                      <Option value="name">Name</Option>
+                    </Select>
+                  </div>
+                  <div className="flex flex-col">
+                    <div className="flex items-center">
+                      <button
+                        onClick={() => setSortOrder(sortOrder === "asc" ? "desc" : "asc")}
+                        className="flex items-center justify-center w-10 h-10 mt-1 border rounded"
+                      >
+                        {sortOrder === "asc" ? (
+                          <BarsArrowUpIcon className="h-6 w-6" />
+                        ) : (
+                          <BarsArrowDownIcon className="h-6 w-6" />
+                        )}
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="flex-col justify-center mx-4 space-y-2">
+                <Button
+                  onClick={toggleDrawer}
+                  variant="outlined"
+                  className=" rounded-md border-gray-400"
+                  fullWidth
+                >
+                  Apply Filters
+                </Button>
+                <Button
+                  onClick={() => {
+                    setFilterName("")
+                    setFilterType("")
+                    setFilterCategory("")
+                    setSortOrder("asc")
+                    setSortField("price")
+                  }}
+                  variant="outlined"
+                  className="rounded-md border-gray-400"
+                  fullWidth
+                >
+                  Reset Filters
+                </Button>
+              </div>
+            </Drawer>
+            <div className="hidden sm:flex flex-wrap place-content-end gap-3">
+              <div className="flex flex-col min-w-36 w-40">
+                <Input
+                  id="filter-name"
+                  type="text"
+                  value={filterName}
+                  onChange={(e) => setFilterName(e.target.value)}
+                  className=""
+                  variant="standard"
+                  labelProps={{ 
+                    className: "after:border-b-[1px] min-w-32 w-32"
+                  }}
+                  containerProps={{
+                    className: "min-w-32 w-32"
+                  }}
+                  label="Search"
+                />
+              </div>
+              <div className="flex flex-col">
+                <Select
+                  id="filter-category"
+                  value={filterCategory}
+                  onChange={(e) => setFilterCategory(e)}
+                  className="pt-1 "
+                  variant="standard"
+                  labelProps={{ 
+                    className: "after:border-b-[1px]"
+                  }}
+                  containerProps={{
+                    className: "min-w-32 w-32"
+                  }}
+                  menuProps={{
+                    className: "p-0"
+                  }}
+                  label="Category"
+                >
+                  <Option value="">All Categories</Option>
+                  <Option value="Pendants">Pendants</Option>
+                  <Option value="Rings">Rings</Option>
+                </Select>
+
+                {/*
+                  Bug with material-tailwind where dynamically populated
+                  options are not displayed when selected.
+
+                  https://github.com/creativetimofficial/material-tailwind/issues/501
+                  https://github.com/creativetimofficial/material-tailwind/issues/513
+
+                  Below is a workaround that works by forcing rerender,
+                  but this causes unsightly flickering so I'm hardcoding the options instead.
+                */}
+                {/* <AsyncSelect
+                  id="filter-category"
+                  value={filterCategory}
+                  onChange={(e) => setFilterCategory(e ?? "")}
+                  className="pt-1  transform-none transition-none animate-none"
+                  variant="standard"
+                  labelProps={{ 
+                    className: "after:border-b-[1px] transform-none transition-none animate-none"
+                  }}
+                  containerProps={{
+                    className: "min-w-32 w-32 transform-none transition-none animate-none"
+                  }}
+                  menuProps={{
+                    className: "p-0 transform-none transition-none animate-none"
+                  }}
+                  label="Category"
+                >
+                  {Array.isArray(categories) && [
+                    <Option key="all" value="">All Categories</Option>,
+                    ...categories.map(category => (
+                      <Option key={category.id} value={category.name}>{category.name}</Option>
+                    ))
+                  ]}
+                </AsyncSelect> */}
+
+              </div>
+              <div className="flex flex-row mb-4 space-x-2">
+                <div className="flex flex-col ">
+                  <Select
+                    id="sort-field"
+                    value={sortField}
+                    onChange={(e) => setSortField(e)}
+                    className=""
+                    variant="standard"
+                    labelProps={{ 
+                      className: "after:border-b-[1px]"
+                    }}
+                    containerProps={{
+                      className: "min-w-40 w-40 sm:min-w-[69px] sm:w-[69px]"
+                    }}
+                    menuProps={{
+                      className: "p-0"
+                    }}
+                    label="Sort by"
+                  >
+                    <Option value="price">Price</Option>
+                    <Option value="name">Name</Option>
+                  </Select>
+                </div>
+                <div className="flex flex-col">
+                  <div className="flex items-center">
+                    <button
+                      onClick={() => setSortOrder(sortOrder === "asc" ? "desc" : "asc")}
+                      className="flex items-center justify-center w-10 h-10 mt-1 border rounded"
+                    >
+                      {sortOrder === "asc" ? (
+                        <BarsArrowUpIcon className="h-6 w-6" />
+                      ) : (
+                        <BarsArrowDownIcon className="h-6 w-6" />
+                      )}
+                    </button>
+                  </div>
+                </div>
+                <div className="flex flex-col">
+                  <Button
+                    onClick={() => {
+                      setFilterName("")
+                      setFilterType("")
+                      setFilterCategory("")
+                      setSortOrder("asc")
+                      setSortField("price")
+                    }}
+                    className="w-10 max-w-10 px-[2px] mx-0 mt-1 rounded border-gray-200 shadow-none"
+                    variant="outlined"
+                  >
+                    {/* <div className="flex items-center"> */}
+                      <Typography className="text-[10px] p-0 m-0">Reset</Typography>
+                      {/* <Bars3Icon className="h-6 w-6" /> */}
+                    {/* </div> */}
+                  </Button>
+              </div>
+              </div>
             </div>
-          </Link>
-          <Link to="/treasures/rings">
-            <div className="bg-white border aspect-square w-48 relative rounded hover:scale-[1.03] transition-transform">
-              <img src="ring_1.png" className=""></img>
-              <div className="text-center font-semibold">Rings</div>
-            </div>
-          </Link>
+          </div>
+          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 mt-4">
+            {filteredProducts.map((product) => (
+              <div className="grid col-span-1" key={product.url_key}>
+                <Link to={`/treasure/${product.url_key}`}>
+                  <div className="bg-gray-50 border aspect-square relative rounded hover:scale-[1.03] transition-transform">
+                    <ProgressiveImage
+                      thumbnailSrc={`/public/products/${product.sku}/thumbnails/${product.images?.[0]?.replace(/(\.[^.]+)$/, '_thumbnail$1')}`}
+                      hdSrc={`/public/products/${product.sku}/${product.images?.[0]}`}
+                      alt={product.name}
+                      spinner={false}
+                    />
+                    <div className="p-2">
+                      <div className="text-center font-semibold">{product.name}</div>
+                      <div className="text-center font-semibold">£{product.price}</div>
+                    </div>
+                  </div>
+                </Link>
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
+      ) : (
+        <CategorySkeleton showFilters />
+      )}
+    </>
   )
 }
 
