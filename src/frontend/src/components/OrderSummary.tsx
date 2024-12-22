c78d9f23f4fb220c2dece76580f9a98ffcb0e12c
Ayman <asuleihi@gmail.com>
2024-12-22 18:21:10 +0000

    Fix infinite skeleton when cart empty

--- src/frontend/src/components/OrderSummary.tsx
+++ src/frontend/src/components/OrderSummary.tsx
@@ -68,111 +68,109 @@ const OrderSummary: React.FC<OrderSummaryProps> = ({ editable }: OrderSummaryPro
 
   return (
     <>
-      {products.length > 0 ? (
+      {cartItems.length === 0 ? (
+        <div className="my-4 font-semibold text-center">Cart Empty</div>
+      ) : products.length > 0 ? (
         <div className="">
-          {cartItems.length > 0 ? 
-            <div className="w-full">
-              <div className="flex border-t w-full">
-                <ul className="w-full">
-                  {products.map(product => (
-                    <li key={product.id} className="py-4 border-b">
-                      <div className="flex">
-                        <div className="flex w-1/6 min-w-28 mr-8">
-                          <Link to={`/treasure/${product.url_key}`}>
-                            <ProgressiveImage
-                              thumbnailSrc={`/public/products/${product.sku}/thumbnails/${product.images?.[0]?.replace(/(\.[^.]+)$/, '_thumbnail$1')}`}
-                              hdSrc={`/public/products/${product.sku}/${product.images?.[0]}`}
-                              alt={product.name}
-                              className="h-28 w-28"
-                              spinner={false}
-                            />
-                          </Link>
+          <div className="w-full">
+            <div className="flex border-t w-full">
+              <ul className="w-full">
+                {products.map(product => (
+                  <li key={product.id} className="py-4 border-b">
+                    <div className="flex">
+                      <div className="flex w-1/6 min-w-28 mr-8">
+                        <Link to={`/treasure/${product.url_key}`}>
+                          <ProgressiveImage
+                            thumbnailSrc={`/public/products/${product.sku}/thumbnails/${product.images?.[0]?.replace(/(\.[^.]+)$/, '_thumbnail$1')}`}
+                            hdSrc={`/public/products/${product.sku}/${product.images?.[0]}`}
+                            alt={product.name}
+                            className="h-28 w-28"
+                            spinner={false}
+                          />
+                        </Link>
+                      </div>
+                      <div className="flex flex-col md:flex-row w-4/6">
+                        <div className="flex items-center md:w-1/2 md:mr-10">
+                          <Link to={`/treasure/${product.url_key}`} className="w-full font-semibold">{product.name}</Link>
                         </div>
-                        <div className="flex flex-col md:flex-row w-4/6">
-                          <div className="flex items-center md:w-1/2 md:mr-10">
-                            <Link to={`/treasure/${product.url_key}`} className="w-full font-semibold">{product.name}</Link>
-                          </div>
-                          <div className="flex font-semibold items-center md:w-1/2 mt-auto md:mt-0">
-                            <div className="mr-auto">
-                              {editable && 
-                                <Button
-                                  variant="text"
-                                  size="sm"
-                                  className="p-3 mb-px hover:bg-transparent active:bg-transparent"
-                                  ripple={false}
-                                  onClick={() => decreaseQuantity(product.id)}
-                                  disabled={cartItems.find(item => item.id === product.id)?.quantity == 1}
-                                >
-                                  <FontAwesomeIcon
-                                    icon={faMinus}
-                                  />
-                                </Button>
-                              }
-                              {cartItems.find(item => item.id === product.id)?.quantity}
-                              {editable && 
-                                <Button
-                                  variant="text"
-                                  size="sm"
-                                  className="p-3 mb-px hover:bg-transparent active:bg-transparent"
-                                  ripple={false}
-                                  onClick={() => increaseQuantity(product.id)}
-                                  disabled={(cartItems.find(item => item.id === product.id)?.quantity || 0) >= product.quantity}
-                                >
-                                  <FontAwesomeIcon
-                                    icon={faPlus}
-                                  />
-                                </Button>
-                              }
-                            </div>
-                          </div>
-                        </div>
-                        <div className="flex flex-col-reverse md:flex-row w-1/6 items-center">
-                          <div className="ml-auto">
-                            <div className="font-semibold ml-auto mb-auto">
-                              £{((cartItems.find(item => item.id === product.id)?.quantity || 0) * product.price).toFixed(2)}
-                            </div>
-                          </div>
-                          {editable && 
-                            <div className="items-center ml-auto mb-auto md:mb-0">
+                        <div className="flex font-semibold items-center md:w-1/2 mt-auto md:mt-0">
+                          <div className="mr-auto">
+                            {editable && 
                               <Button
                                 variant="text"
                                 size="sm"
-                                className="mb-px p-3 pt-1 md:pt-3 hover:bg-transparent active:bg-transparent"
+                                className="p-3 mb-px hover:bg-transparent active:bg-transparent"
                                 ripple={false}
-                                onClick={() => removeFromCart(product.id)}
+                                onClick={() => decreaseQuantity(product.id)}
+                                disabled={cartItems.find(item => item.id === product.id)?.quantity == 1}
                               >
-                                <FontAwesomeIcon icon={faXmark} />
+                                <FontAwesomeIcon
+                                  icon={faMinus}
+                                />
                               </Button>
-                            </div>
-                          }
+                            }
+                            {cartItems.find(item => item.id === product.id)?.quantity}
+                            {editable && 
+                              <Button
+                                variant="text"
+                                size="sm"
+                                className="p-3 mb-px hover:bg-transparent active:bg-transparent"
+                                ripple={false}
+                                onClick={() => increaseQuantity(product.id)}
+                                disabled={(cartItems.find(item => item.id === product.id)?.quantity || 0) >= product.quantity}
+                              >
+                                <FontAwesomeIcon
+                                  icon={faPlus}
+                                />
+                              </Button>
+                            }
+                          </div>
                         </div>
                       </div>
-                    </li>
-                  ))}
-                </ul>
+                      <div className="flex flex-col-reverse md:flex-row w-1/6 items-center">
+                        <div className="ml-auto">
+                          <div className="font-semibold ml-auto mb-auto">
+                            £{((cartItems.find(item => item.id === product.id)?.quantity || 0) * product.price).toFixed(2)}
+                          </div>
+                        </div>
+                        {editable && 
+                          <div className="items-center ml-auto mb-auto md:mb-0">
+                            <Button
+                              variant="text"
+                              size="sm"
+                              className="mb-px p-3 pt-1 md:pt-3 hover:bg-transparent active:bg-transparent"
+                              ripple={false}
+                              onClick={() => removeFromCart(product.id)}
+                            >
+                              <FontAwesomeIcon icon={faXmark} />
+                            </Button>
+                          </div>
+                        }
+                      </div>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="flex font-semibold flex-col max-w-[432px] ml-auto mt-5">
+              <div className="flex place-content-between">
+                <div className="flex">Subtotal</div>
+                <div className="flex">£{orderTotal.toFixed(2)}</div>
               </div>
-              <div className="flex font-semibold flex-col max-w-[432px] ml-auto mt-5">
-                <div className="flex place-content-between">
-                  <div className="flex">Subtotal</div>
-                  <div className="flex">£{orderTotal.toFixed(2)}</div>
-                </div>
-                <div className="flex place-content-between">
-                  <div className="flex">Shipping</div>
-                  <div className="flex">£0.00</div>
-                </div>
-                <div className="flex place-content-between">
-                  <div className="flex">Tax</div>
-                  <div className="flex">£0.00</div>
-                </div>
-                <div className="flex place-content-between border-t py-1">
-                  <div className="flex">Order Total</div>
-                  <div className="flex">£{orderTotal.toFixed(2)}</div>
-                </div>
+              <div className="flex place-content-between">
+                <div className="flex">Shipping</div>
+                <div className="flex">£0.00</div>
+              </div>
+              <div className="flex place-content-between">
+                <div className="flex">Tax</div>
+                <div className="flex">£0.00</div>
+              </div>
+              <div className="flex place-content-between border-t py-1">
+                <div className="flex">Order Total</div>
+                <div className="flex">£{orderTotal.toFixed(2)}</div>
               </div>
             </div>
-            :
-            <div className="my-4 font-semibold text-center">Cart Empty</div>
-          }
+          </div>
         </div>
       ) : (
         <OrderSummarySkeleton itemCount={cartItems.length} />
