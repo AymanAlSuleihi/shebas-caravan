fc6580ddf9f56149cb414730cf6338aab32c604b
Ayman <asuleihi@gmail.com>
2024-12-22 17:08:17 +0000

    Add includeTime param

--- src/frontend/src/utils/utils.ts
+++ src/frontend/src/utils/utils.ts
@@ -1,6 +1,9 @@
-const formatDate = (date?: string) => {
+const formatDate = (date?: string, includeTime: boolean = false) => {
   if (date) {
     const dateString = new Date(date)
+    if (includeTime) {
+      return dateString.toISOString().replace('T', ' ').split('.')[0]
+    }
     return dateString.toISOString().split('T')[0]
   }
   return null
