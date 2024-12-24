46cb47cfe9fc93e43267260f200a6e4ffa07090a
Ayman <asuleihi@gmail.com>
2024-12-24 22:55:51 +0000

    Add import for shipping zone model

--- src/backend/app/alembic/env.py
+++ src/backend/app/alembic/env.py
@@ -25,6 +25,7 @@ from app.models.generic import ProductCategoryLink
 from app.models.category import Category
 from app.models.product import Product
 from app.models.user import User
+from app.models.shipping_zone import ShippingZone
 # from sqlmodel import SQLModel
 # from app import models
 # from models import *
