ae0f395ae9b7756b8126bbb99885afde0b8254cd
Ayman <asuleihi@gmail.com>
2024-12-31 00:00:38 +0000

    Add migrations

--- src/backend/app/alembic/env.py
+++ src/backend/app/alembic/env.py
@@ -26,6 +26,7 @@ from app.models.category import Category
 from app.models.product import Product
 from app.models.user import User
 from app.models.shipping_zone import ShippingZone
+from app.models.currency import Currency
 # from sqlmodel import SQLModel
 # from app import models
 # from models import *
