a5b04c16911cbb2fb2cd67e8a125ec34e151b698
Ayman <asuleihi@gmail.com>
2024-12-31 00:00:21 +0000

    Add currency

--- src/backend/app/app/api/api_v1/api.py
+++ src/backend/app/app/api/api_v1/api.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 
 from app.api.api_v1.endpoints import (
-    customers,
     carts,
     categories,
+    currency,
+    customers,
     login,
     media,
     orders,
@@ -21,6 +22,7 @@ from app.api.api_v1.endpoints import (
 api_router = APIRouter()
 api_router.include_router(carts.router, prefix="/carts", tags=["carts"])
 api_router.include_router(categories.router, prefix="/categories", tags=["categories"])
+api_router.include_router(currency.router, prefix="/currency", tags=["currency"])
 api_router.include_router(customers.router, prefix="/customers", tags=["customers"])
 api_router.include_router(login.router, tags=["login"])
 api_router.include_router(media.router, prefix="/media", tags=["media"])
