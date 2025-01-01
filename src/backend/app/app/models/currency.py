1370ce976330f638d063a62a6d91d2a535935f4c
Ayman <asuleihi@gmail.com>
2025-01-01 23:00:30 +0000

    Add currency symbol and stripe processing info

--- src/backend/app/app/models/currency.py
+++ src/backend/app/app/models/currency.py
@@ -12,6 +12,10 @@ class CurrencyBase(SQLModel):
     target_code: str
     timestamp: datetime
     rate: float
+    symbol: str
+    stripe_supported: bool = False
+    stripe_zero_decimal: bool = False
+    stripe_no_fraction: bool = False
 
 
 class CurrencyCreate(CurrencyBase):
@@ -23,6 +27,10 @@ class CurrencyUpdate(SQLModel):
     target_code: Optional[str] = None
     timestamp: Optional[datetime] = None
     rate: Optional[float] = None
+    symbol: Optional[str] = None
+    stripe_supported: Optional[bool] = None
+    stripe_zero_decimal: Optional[bool] = None
+    stripe_no_fraction: Optional[bool] = None
 
 
 class Currency(
