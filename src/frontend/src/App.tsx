b2bf90ec8ace84af84f7b70aef5a01573d7d4532
Ayman <asuleihi@gmail.com>
2025-01-03 23:40:25 +0000

    Add dark mode provider

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,28 +1,31 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import { Refine } from '@refinedev/core'
+import React from "react"
+import { Outlet } from "react-router-dom"
+import { Refine } from "@refinedev/core"
 
-import { Navbar } from './components/Navbar'
-import { Footer } from './components/Footer'
-import { ShoppingCartProvider } from './context/shoppingCartContext'
-import { dataProvider } from './providers/DataProvider'
-import ScrollToTop from './components/ScrollToTop'
-import { CurrencyProvider } from './context/CurrencyContext'
+import { Navbar } from "./components/Navbar"
+import { Footer } from "./components/Footer"
+import { ShoppingCartProvider } from "./contexts/ShoppingCartContext"
+import { dataProvider } from "./providers/DataProvider"
+import ScrollToTop from "./components/ScrollToTop"
+import { CurrencyProvider } from "./contexts/CurrencyContext"
+import { DarkModeProvider } from "./contexts/DarkModeContext"
 
 const App: React.FC = () => {
   return (
     <>
       <Refine dataProvider={dataProvider}>
-        <CurrencyProvider>
-          <ShoppingCartProvider>
-            <div className="flex flex-col min-h-screen bg-gray-50">
-              <Navbar />
-              <ScrollToTop />
-              <Outlet />
-              <Footer />
-            </div>
-          </ShoppingCartProvider>
-        </CurrencyProvider>
+        <DarkModeProvider>
+          <CurrencyProvider>
+            <ShoppingCartProvider>
+              <div className="flex flex-col min-h-screen bg-gray-50">
+                <Navbar />
+                <ScrollToTop />
+                <Outlet />
+                <Footer />
+              </div>
+            </ShoppingCartProvider>
+          </CurrencyProvider>
+        </DarkModeProvider>
       </Refine>
     </>
   )
