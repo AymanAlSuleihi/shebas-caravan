dbb57e4b4aa7d285d5c3ae0fd53f55b19177a3b9
Ayman <asuleihi@gmail.com>
2025-01-11 00:02:04 +0000

    Add optional color prop

--- src/frontend/src/components/AlertMessage.tsx
+++ src/frontend/src/components/AlertMessage.tsx
@@ -1,17 +1,18 @@
 // AlertMessage.tsx
 import React, { useEffect, useState } from "react"
 import { Alert } from "@material-tailwind/react"
-import type { variant } from "@material-tailwind/react/types/components/alert"
+import type { color, variant } from "@material-tailwind/react/types/components/alert"
 
 interface AlertMessageProps {
   children: React.ReactNode
   variant: variant
+  color?: color
   className: string
   timeout: number
   onClose?: () => void
 }
 
-export const AlertMessage: React.FC<AlertMessageProps> = ({ children, variant, className, timeout, onClose }) => {
+export const AlertMessage: React.FC<AlertMessageProps> = ({ children, variant, color, className, timeout, onClose }) => {
   const [open, setOpen] = useState(true)
 
   useEffect(() => {
@@ -24,6 +25,6 @@ export const AlertMessage: React.FC<AlertMessageProps> = ({ children, variant, c
   }, [])
 
   return (
-    <Alert open={open} variant={variant} className={className}>{children}</Alert>
+    <Alert open={open} variant={variant} color={color} className={className}>{children}</Alert>
   )
 }
