8c82bb60b36c870f2ff21db693d7d14ef5024860
Ayman <asuleihi@gmail.com>
2025-01-15 00:52:08 +0000

    Add react typing / material-tailwind bug workaround

--- src/frontend/src/vite-env.d.ts
+++ src/frontend/src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+import * as React from "react"
+
+declare module "react" {
+  interface HTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
+    placeholder?: string
+    onPointerEnterCapture?: React.PointerEventHandler<T>
+    onPointerLeaveCapture?: React.PointerEventHandler<T>
+    crossOrigin?: string
+  }
+}
