36fc68f034de722ac6a415ff10d66b6fdbf095e4
Ayman <asuleihi@gmail.com>
2025-01-17 23:23:27 +0000

    Add material-tailwind @types/react compatibility issues workaround

--- src/frontend/src/vite-env.d.ts
+++ src/frontend/src/vite-env.d.ts
@@ -10,3 +10,25 @@ declare module "react" {
     crossOrigin?: string
   }
 }
+
+import {} from "@material-tailwind/react"
+
+type EventCapture = {
+  onPointerEnterCapture?: unknown
+  onPointerLeaveCapture?: unknown
+}
+
+declare module "@material-tailwind/react" {
+  export interface ButtonProps extends EventCapture {
+    placeholder?: unknown
+  }
+  export interface InputProps extends EventCapture {
+    crossOrigin?: unknown
+  }
+  export interface SelectProps extends EventCapture {
+    placeholder?: unknown
+  }
+  export interface SpinnerProps extends EventCapture {
+    placeholder?: unknown
+  }
+}
